6121578ec7273b992009a032cd7f96626de52d3e
SHINU VARGHESE <72874745+SHINUVARGHESE@users.noreply.github.com>
2021-03-23 08:49:01 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,59 +1,86 @@
-/*$(document).ready(function(){
-    $("#submit-form").validate({
-        rules: {
-           
-            mobile:{
-                required: true,
-                minlength : 10,
-                digit:true
-            },
-            mail:{
-                required: true,
-                email:true
-            },
-            content:{
-                required:true
 
-            }
-        },
-        messages: {
-            name:{
-                required:"Please enter your name"
-            },
-            mobile:{
-                required:"Please enter valid number"
-            },
-            content:{
-                required:"Please enter Your message"
-            }
-        }
-    })
-   
-*/
-
-    
-$("#submit-form").submit((e) => {
-    e.preventDefault()
+  jQuery.validator.addMethod("noSpace", function(value, element) { 
+    return value == '' || value.trim().length != 0;  
+  }, "No space please and don't leave it empty");
+  jQuery.validator.addMethod("customEmail", function(value, element) { 
+  return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value ); 
+  }, "Please enter valid email address!");
+  $.validator.addMethod("numeric", function(value, element) {
+    return this.optional(element) || value == value.match(/^[0-9]+$/);
+  }, "Numbers Only");
+  $.validator.addMethod( "alphanumeric", function( value, element ) {
+  return this.optional( element ) || value == value.match(/^[a-zA-Z\s]+$/);
+  }, "Letters only" );
+  
+  var $registrationForm = $('#submit-form');
+  
+  if($registrationForm.length){
+  $registrationForm.validate({
+      rules:{
+          //username is the name of the textbox
+          name: {
+              required: true,
+              //alphanumeric is the custom method, we defined in the above
+              minlength:4,
+              alphanumeric: true
+          },
+          mail: {
+              required: true,
+              customEmail: true
+          },
+        
+          mobile: {
+              required: true,
+              maxlength:10,
+              numeric:true,
+              noSpace: true
+          },
+        
+          content: {
+              required: true
+          }
+      },
+      messages:{
+          name: {
+              //error message for the required field
+              required: 'Please Enter Your Name'
+          },
+          mail: {
+              required: 'Please Enter Your Email Id',
+              //error message for the email field
+              mail: 'Please Enter Valid Email Id'
+          },
+       
+          mobile: {
+              required: 'Please Enter  Your Phone Number'
+          },
+        
+          content: {
+              required: 'Please Enter Your message'
+          }
+      },
+      submitHandler: function(form) {
+        // form.submit();   //  commented this
+        submitForm();       //  ADDED this call
+      }
+  }) 
+       
+  }
+  
+  function submitForm() {
+  
     $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbwWOuOQLHLrAqVFMDVGDVLxs25E-wtAteSiLXF1A8QB9-NlgiHCkhd268K-cVc1BOlG2Q/exec",
-        data: $("#submit-form").serialize(), 
-        method: "post",
-        success: function (response) {
+        url:"https://script.google.com/macros/s/AKfycbwWOuOQLHLrAqVFMDVGDVLxs25E-wtAteSiLXF1A8QB9-NlgiHCkhd268K-cVc1BOlG2Q/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
             alert("Form submitted successfully")
             window.location.reload()
-            //window.location.href="https://google.com"
+            // window.location.href="https://google.com"
         },
-        error: function (err) {
+        error:function (err){
             alert("Something Error")
-
+  
         }
     })
-})
-
-
-
-
-
-    
-
-
+  }
